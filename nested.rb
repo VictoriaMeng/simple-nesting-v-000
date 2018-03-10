fde9f996da889401a5fc84c7d3730c30c471bf87
Victoria Meng <nights1ststar@protonmail.com>
2018-03-10 23:15:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -61,10 +61,6 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash =
  		{
@@ -82,7 +78,9 @@ def adding_matz
         }
      }
 
-
+	programmer_hash[:yukihiro_matsumoto] = {}
+	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+	programmer_hash[:languages] = %w(LISP C)
 end
 
 def changing_alan
