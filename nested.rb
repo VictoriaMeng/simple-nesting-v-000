b7ff544113ddf839c123df9f94e6ddd3c966f4ad
Victoria Meng <nights1ststar@protonmail.com>
2018-03-10 23:20:05 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -80,6 +80,12 @@ def adding_matz
 
 	programmer_hash[:yukihiro_matsumoto] = {}
 	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+	programmer_hash[:yukihiro_matsumoto][:languages] = %w(LISP C)
+
+		 :yukihiro_matsumoto => {
+		   :known_for => "Ruby",
+		    :languages => ["LISP", "C"]
+		 }
 end
 
 def changing_alan
